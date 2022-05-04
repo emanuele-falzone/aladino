4bc172d0f769ed526325837d94ae5036c29360a9
Emanuele Falzone <emafalzo@gmail.com>
2022-05-04 13:19:44 +0200

    Implement support client for different king of tests

---
+++ test/worlds/support/Client.ts
@@ -0,0 +1,42 @@
+import { Vehicle } from "../../../src/domain/Vehicle";
+
+export class Client {
+    private vehicleList: Vehicle[] = [];
+    private error? : Error;
+    
+    receiveVehicleList(vehicleList: Vehicle[]) {
+        this.vehicleList = vehicleList
+    }
+
+    confirmVehicleListIsEmpty() {
+        assert(this.vehicleList.length == 0, "The vehicle is not empty");
+    }
+
+    confirmVehicleListContains(name: string, plate: string) {
+        let result = false;
+        for (let vehicle of this.vehicleList)          {
+            if (vehicle.name == name && vehicle.plate == plate) {
+                result = true;
+            }
+        }
+        assert(result, `The vehicle list does not contain the vehicle ${name} with plate ${plate}`)
+    }
+
+    confirmVehicleListSize(length: number) {
+        assert(this.vehicleList.length == length, `The vechicle list is not ${length} long: ${this.vehicleList}`)
+    }
+
+    confirmError() {
+        assert(!!this.error, "No error has been thrown")
+    }
+
+    receiveError(error: Error) {
+        this.error = error;
+    }
+}
+
+function assert(value: boolean, message: string) {
+    if (!value) {
+        throw new Error(message);
+    }
+}
